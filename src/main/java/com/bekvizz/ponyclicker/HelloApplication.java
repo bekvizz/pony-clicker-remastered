8cfb5ea1a611aac7c447b57289ee65a614955884
bekvizz <bekvizzyt@gmail.com>
2025-06-04 15:29:29 +0300

    Initial commit

--- src/main/java/com/bekvizz/ponyclicker/HelloApplication.java
+++ src/main/java/com/bekvizz/ponyclicker/HelloApplication.java
@@ -1,19 +1,48 @@
 package com.bekvizz.ponyclicker;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private final Image icon = new Image(getClass().getResourceAsStream("/icons/icon_ph.png"));
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        Label label = new Label();
+        Button btn1 = new Button("Say hi");
+
+
+
+        StackPane root = new StackPane();
+        StackPane.setAlignment(btn1, Pos.CENTER);
+        btn1.setOnAction(event -> {
+            StackPane.setAlignment(label, Pos.TOP_CENTER);
+            StackPane.setMargin(label, new Insets(15d));
+            label.setBackground(Background.fill(Paint.valueOf("#bababa")));
+            label.setText("Hello!");
+        });
+
+
+        root.getChildren().add(btn1);
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300d,100d);
         stage.setScene(scene);
+        stage.setTitle("App");
+        stage.getIcons().add(icon);
         stage.show();
     }
 
